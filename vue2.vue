a98b1ba795d26ad83081e319f8f78ea8efb84e17
S <rock59538@gmail.com>
2019-04-19 09:32:47 +0300

    first commit

---
+++ vue2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Пробный текст</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue2"
+    }
+</script>
+
+<style scoped>
+
+</style>
